811906df618cd209e83deefd4d9fc8c873f41dbe
Benjamin Swanzey <swanzeyb2001@gmail.com>
2024-12-02 23:36:55 -0800

    Add scope flag

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 import PostScriptInterpreter from './PostScriptInterpreter'
 
-const interpreter = new PostScriptInterpreter({ scoping: 'dynamic' })
+const use_static = process.argv.includes('--static')
+
+console.log(`Using ${use_static ? 'static' : 'dynamic'} scoping`)
+
+const interpreter = new PostScriptInterpreter({
+  scoping: use_static ? 'static' : 'dynamic',
+})
+
 interpreter.repl()
